1556320ceda483c03d13cc01eae6acc9c14ce57a
Antwan van Houdt <a.vanhoudt@wearetriple.com>
2020-10-23 10:32:35 +0200

    Added more included headers for libavutil

--- Sources/avutil/shim.h
+++ Sources/avutil/shim.h
@@ -2,5 +2,13 @@
 #define AVUTIL_H
 
 #include <libavutil/avutil.h>
+#include <libavutil/dict.h>
+#include <libavutil/eval.h>
+#include <libavutil/fifo.h>
+#include <libavutil/hwcontext.h>
+#include <libavutil/pixfmt.h>
+#include <libavutil/rational.h>
+#include <libavutil/thread.h>
+#include <libavutil/threadmessage.h>
 
 #endif
